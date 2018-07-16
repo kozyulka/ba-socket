c2e27a5b78f5579d99d21bc8957e7bdb2ea0e098
Daryna Gavrylenko <deakozyulka@gmail.com>
2018-07-16 19:55:00 +0300

    refactor message adding

--- client/index.js
+++ client/index.js
@@ -4,84 +4,55 @@ const socket = io.connect();
 const messagesContainer = document.getElementById('chatMessages');
 const messageInput = document.getElementById('myMessage');
 const messages = [];
-const outgoingMessages = [];
+
+const addMessage = (text, sender) => {
+    const message = {
+        text,
+        sender,
+        time: moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+    };
+
+    messages.push(message);
+};
+
+const showMessages = () => {
+    const html = messages
+        .map((message) => {
+            let className = 'chat-message';
+
+            if (message.sender === 'me') {
+                className += ' outgoing';
+            }
+
+            return `
+                <div class="chat-message-wrapper">
+                    <div class="${className}">
+                        <div class="chat-message-info">
+                            <div class="chat-message-info-sender">${message.sender}</div>
+                            <div class="chat-message-info-time">${message.time}</div>
+                        </div>
+                        <div class="chat-message-text">${message.text}</div>
+                    </div>
+                </div>
+            `;
+        })
+        .join('');
+
+    messagesContainer.innerHTML = html;
+};
 
 const sendMessage = () => {
     const messageText = messageInput.value;
     messageInput.value = '';
 
     socket.emit('message', messageText);
-    addOutgoingMessage(messageText);
-    showOutgoingMessages(messageText);
-};
-
-const addOutgoingMessage = (text) => {
-    let outgoingMessage = {
-        sender: 'me',
-        time: Date.now(),
-        text: text
-    };
-
-    outgoingMessages.push(outgoingMessage);
-};
-
-const addMessage = (text) => {
-    let message = {
-        sender: 'not me',
-        time: Date.now(),
-        text: text
-    };
-
-    messages.push(message);
-};
-
-const showOutgoingMessages = () => {
-
-    let html = outgoingMessages
-    .map((outgoingMessage) => {
-            return `
-            <div class="chat-message-wrapper">
-                <div class="chat-message outgoing">
-                    <div class="chat-message-info">
-                         <div class="chat-message-info-sender">${outgoingMessage.sender}</div>
-                         <div class="chat-message-info-time">${outgoingMessage.time}</div>
-                    </div>
-                    <div class="chat-message-text">${outgoingMessage.text}</div>
-                </div>
-             </div>
-            `;
-    })
-    .join('');
-
-
-    messagesContainer.innerHTML = html;
-};
-
-const showMessages = () => {
-
-    let html = messages
-    .map((message) => {
-        return
-        `
-        <div class="chat-message-wrapper">
-            <div class="chat-message">
-                <div class="chat-message-info">
-                     <div class="chat-message-info-sender">${message.sender}</div>
-                     <div class="chat-message-info-time">${message.time}</div>
-                </div>
-                <div class="chat-message-text">${message.text}</div>
-            </div>
-         </div>
-        `;
-    })
-    .join('');
-
-    messagesContainer.innerHTML = html;
+    addMessage(messageText, 'me');
+    showMessages(messageText);
 };
 
 socket.on('message', (messageText) => {
     console.log(messageText);
-    addMessage(messageText);
+    addMessage(messageText, 'user');
     showMessages();
 });
 
